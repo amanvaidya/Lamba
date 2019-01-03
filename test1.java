8b97c881b99a1422e16a68fe39b47b835370bd13
aman <amanvaidya700@gmail.com>
2019-01-03 22:42:05 +0530

    Create test1.java
    This will compile

---
+++ test1.java
@@ -0,0 +1,10 @@
+interface Inter{
+void Fun(int x, int y);
+}
+
+public class test1{
+public static void main (String args[]){
+Inter inter=(int x, int y)->System.out.println(x+y);
+inter.Fun(3,4);
+}
+}
